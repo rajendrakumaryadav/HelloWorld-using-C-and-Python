a701b30dead7cbe5d3893d8f09a4f007461ade7a
Rajendra Kumar Yadav <rajendra.ecti@gmail.com>
2020-09-23 21:04:41 +0530

    Added

--- Class1.cs
+++ Class1.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using System;  
+class Class1 
+{  
+    public static string Message()  
+    {  
+        return "This method returns string, directly or maybe after some manipulations.";  
+    }  
+    public static int method2()  
+    {  
+        Console.WriteLine("I am Int, how someone can forget including me in in any sample application ^_^");  
+        return 1 * 2 * 3 * 4 * 5;  
+    }  
+    public static double method3()  
+    {  
+        Console.WriteLine("Trust me, I am frequetly useable but in complex calculations you need me always ^_^");  
+        return (0.1 * 5) + (8 / 9) - (34 ^ 2) * 0.01;  
+    }  
 
-public class Class1
-{
-    public int Add(int lnum, int rnum) {
-        return lnum + rnum;
-    }
-    public int Sub(int lnum, int rnum) {
-        return rnum - lnum;
-    }
 }
