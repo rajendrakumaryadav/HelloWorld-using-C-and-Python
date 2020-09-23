6fe21add4df220636f3f56dd8f3be97ba9a3cb80
Rajendra Kumar Yadav <rajendra.ecti@gmail.com>
2020-09-23 21:06:56 +0530

    Added

--- hello_python.py
+++ hello_python.py
@@ -1,4 +1,16 @@
-from ctypes import *
-lib = cdll.LoadLibrary(
-    ".\\bin\\Debug/netcoreapp3.1\\HelloWorld.dll")
-print(lib.Add(10, 20))
+import clr  
+#Adding reference to class library!  
+clr.AddReference('.\\bin\\Debug\\netcoreapp3.1\\HelloWorld.dll')
+ 
+#importing specific class from this namespace, here!  
+import Class1
+wrapper = Class1()
+# calling functions using class name, since these all are static!  
+def pmethod1():  
+     str = wrapper.Message()  
+  
+def pmethod2():  
+     inte = wrapper.method2()  
+  
+def pmethod3():  
+    doub= wrapper.method3()  
