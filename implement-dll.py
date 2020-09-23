5df0cf0b2f4136bd9f25b51531e0ba833c3386ce
Rajendra Kumar Yadav <rajendra.ecti@gmail.com>
2020-09-23 20:36:47 +0530

    Initial Commit

--- implement-dll.py
+++ implement-dll.py
@@ -1,4 +1,4 @@
 from ctypes import *
 lib = cdll.LoadLibrary(
-    "/home/raju/Downloads/HelloWorld/bin/Debug/netcoreapp3.1/HelloWorld.dll")
+    "./bin/Debug/netcoreapp3.1/HelloWorld.dll")
 print(lib.Add(10, 20))
